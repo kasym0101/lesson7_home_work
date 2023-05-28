c5148dadb21ae443d2faac0afbca306de684c63f
Kasym0101 <kasym.dzhorobaev@gmail.com>
2023-05-28 13:55:41 +0600

    first commit

---
+++ lesson7_home_work.dart
@@ -0,0 +1,70 @@
+void main() {
+//  1
+  // int i = 1;
+  // while (i <= 100) {
+  //   print(i);
+  //   i++;
+  // }
+  // for (int i = 1; i <= 100; i++) {
+  //   print(i);
+  // }
+
+// 2
+  // int i = 11;
+  // while (i <= 33) {
+  //   print(i);
+  //   i++;
+  // }
+  // for (int i = 11; i <= 33; i++) {
+  //   print(i);
+  // }
+
+// 3
+  // int i = 1;
+  // while (i <= 100) {
+  //   if (i % 2 == 0) {
+  //     print(i);
+  //   }
+  //   i++;
+  // }
+  // for (int i = 1; i <= 100; i++) {
+  //   if (i % 2 == 0) {
+  //     print(i);
+  //   }
+
+  // int i = 0;
+  // while (i <= 100) {
+  //   print(i);
+  //   i += 2;
+  // }
+  // // for (int i = 0; i <= 100; i += 2) {
+  // //   print(i);
+  // // }
+
+  // int i = 0;
+  // while (i <= 100) {
+  //   if (i.isEven) {
+  //     print(i);
+  //   }
+  //   i++;
+  // }
+  // for (int i = 0; i <= 100; i++) {
+  //   if (i.isEven) {
+  //     print(i);
+  //   }
+  // }
+
+// 4
+  // int summa = 0;
+  // int i = 1;
+  // while (i <= 100) {
+  //   summa += i;
+  //   i++;
+  // }
+  // print(summa);
+  // int summa = 0;
+  // for (int i = 1; i <= 100; i++) {
+  //   summa += i;
+  // }
+  // print(summa);
+}
